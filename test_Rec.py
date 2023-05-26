0a6cd9ac5e9ed2d3e82d2538fc254599d805c966
gloriasenatore <gloriasenatore99@gmail.com>
2023-05-26 09:59:26 +0200

    Aggiungo nomi assi all'histo

--- test_Rec.py
+++ test_Rec.py
@@ -2,9 +2,9 @@ import ROOT
 import math
 import numpy as np
 #from matplotlib import pyplot as pl
-from array import array
+from array import array 
 
-hist_pd=ROOT.TH2F("pd","pd",100, 0, 5e4, 100, 0, 5e4)
+hist_pd=ROOT.TH2F("pd","E_delayed vs E_prompt",100, 0, 8, 100, 0, 12)
 f = ROOT.TFile.Open("/storage/gpfs_data/juno/junofs/users/mmalabarba/NMO_spectrum/risultati/all/NO/antinu/root/antinu_rec_user_r14000_0000.root")
 T = f.Get("TRec")
 branch = ["posx", "posy", "posz", "pe_tot", "m_QTtime"]
@@ -16,9 +16,10 @@ pe_prompt=[]
 pe_del=[]
 hist_pe = array('f', [0])
 hist_t = array ('d', [0])
-T.SetBranchAddress("m_PESum", hist_pe)
+T.SetBranchAddress("m_NQE", hist_pe)
 T.SetBranchAddress("m_triggerT", hist_t)
 j=nEntries
+print(str(j))
 for i in range (0,j):
 	T.GetEvent(i)
 	t0=hist_t[0]
@@ -26,15 +27,19 @@ for i in range (0,j):
 	T.GetEvent(i+1)
 	t1=hist_t[0]
 	p1=hist_pe[0]
-	if ((t1-t0)<1e6):
+	if ((t1-t0)<1e7):
 		ind_prompt.append(t0)
 		ind_del.append(t1)
 		pe_prompt.append(p0)
 		pe_del.append(p1)
 		hist_pd.Fill(p1,p0)
-		i+1
-		j-1
+		i=i+1
+		j=j-1
 
 c1 = ROOT.TCanvas("c1", "c1")
 #hist_pd.Fill(pe_prompt,pe_del)
-hist_pd.Draw()
+hist_pd.GetXaxis().SetTitle("E_delayed [MeV]")
+hist_pd.GetYaxis().SetTitle("E_prompt [MeV]")
+hist_pd.Draw("COLZ")
+
+
