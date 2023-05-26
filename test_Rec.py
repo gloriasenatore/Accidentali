00bd08a56d9a930919b8fad0339d60bf9482b606
gloriasenatore <gloriasenatore99@gmail.com>
2023-05-26 10:40:31 +0200

    Sostituzione del ciclo for con while

--- test_Rec.py
+++ test_Rec.py
@@ -4,7 +4,7 @@ import numpy as np
 #from matplotlib import pyplot as pl
 from array import array 
 
-hist_pd=ROOT.TH2F("pd","E_delayed vs E_prompt",100, 0, 8, 100, 0, 12)
+hist_pd=ROOT.TH2F("histo","E_delayed vs E_prompt",100, 0, 8, 100, 0, 12)
 f = ROOT.TFile.Open("/storage/gpfs_data/juno/junofs/users/mmalabarba/NMO_spectrum/risultati/all/NO/antinu/root/antinu_rec_user_r14000_0000.root")
 T = f.Get("TRec")
 branch = ["posx", "posy", "posz", "pe_tot", "m_QTtime"]
@@ -19,27 +19,32 @@ hist_t = array ('d', [0])
 T.SetBranchAddress("m_NQE", hist_pe)
 T.SetBranchAddress("m_triggerT", hist_t)
 j=nEntries
-print(str(j))
-for i in range (0,j):
-	T.GetEvent(i)
-	t0=hist_t[0]
-	p0=hist_pe[0]
-	T.GetEvent(i+1)
-	t1=hist_t[0]
-	p1=hist_pe[0]
-	if ((t1-t0)<1e7):
-		ind_prompt.append(t0)
-		ind_del.append(t1)
-		pe_prompt.append(p0)
-		pe_del.append(p1)
-		hist_pd.Fill(p1,p0)
-		i=i+1
-		j=j-1
+
+i = 0
+while i < j:
+    T.GetEvent(i)
+    t0=hist_t[0]
+    p0=hist_pe[0]
+
+    T.GetEvent(i+1)
+    t1=hist_t[0]
+    p1=hist_pe[0]
+
+    if ((t1-t0)<1e6):
+        ind_prompt.append(t0)
+        ind_del.append(t1)
+        pe_prompt.append(p0)
+        pe_del.append(p1)
+        hist_pd.Fill(p1,p0)
+        i=i+2
+    else:
+        i=i+1
 
 c1 = ROOT.TCanvas("c1", "c1")
 #hist_pd.Fill(pe_prompt,pe_del)
 hist_pd.GetXaxis().SetTitle("E_delayed [MeV]")
 hist_pd.GetYaxis().SetTitle("E_prompt [MeV]")
+hist_pd.SetStats(1)
 hist_pd.Draw("COLZ")
 
 
