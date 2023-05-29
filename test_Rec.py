2bb9bbdb8c112e40ba4d7ed8c99dc19b1ae6a626
gloriasenatore <gloriasenatore99@gmail.com>
2023-05-29 11:24:50 +0200

    Aggiunta la discriminazione tra prompt e delayed anche per posizione (Delta r < 1.5 m)

--- test_Rec.py
+++ test_Rec.py
@@ -15,7 +15,7 @@ for k in range (0, numFile):
     print("Opening file number " + str(k))
 
     T = f.Get("TRec")
-    branch = ["posx", "posy", "posz", "pe_tot", "m_QTtime"]
+    branch = ["recx", "recy", "recz", "m_NQE", "m_triggerT"]
     nEntries = T.GetEntries()
 
     ind_prompt=[]
@@ -24,20 +24,34 @@ for k in range (0, numFile):
     pe_del=[]
     hist_pe = array('f', [0])
     hist_t = array ('d', [0])
+    hist_x = array ('f', [0])
+    hist_y = array ('f', [0])
+    hist_z = array ('f', [0])
     T.SetBranchAddress("m_NQE", hist_pe)
     T.SetBranchAddress("m_triggerT", hist_t)
+    T.SetBranchAddress("recx", hist_x)
+    T.SetBranchAddress("recy", hist_y)
+    T.SetBranchAddress("recz", hist_z)
 
     i = 0
     while i < nEntries:
             T.GetEvent(i)
             t0=hist_t[0]
             p0=hist_pe[0]
+            x0=hist_x[0]
+            y0=hist_y[0]
+            z0=hist_z[0]
+            r0 = (x0**2 + y0**2 + z0**2)**0.5
 
             T.GetEvent(i+1)
             t1=hist_t[0]
             p1=hist_pe[0]
+            x1=hist_x[0]
+            y1=hist_y[0]
+            z1=hist_z[0]
+            r1 = (x1**2 + y1**2 + z1**2)**0.5
 
-            if ((t1-t0)<1e6):
+            if ((t1-t0)<1e6 and abs(r0-r1) < 1500):
                     ind_prompt.append(t0)
                     ind_del.append(t1)
                     pe_prompt.append(p0)
